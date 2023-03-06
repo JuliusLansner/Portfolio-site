d15e5b99d80b55caa7a4c034739d58ea6afc30b7
JuliusLansner <axiiss@live.dk>
2023-03-06 13:00:20 +0100

    work section update

--- frontendreact/src/container/Work/Work.jsx
+++ frontendreact/src/container/Work/Work.jsx
@@ -7,17 +7,17 @@ import { urlFor, client} from '../../client';
 
 import './Work.scss';
 const Work = () => {
-  const [activeFilter, setactiveFilter] = useState('All');
-  const [animateCard, setanimateCard] = useState({y:0, opacity: 1})
-  const [works, setWorks] = useState([])
-  const [filterWork, setfilterWork] = useState([])
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1});
+  const [works, setWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState([]);
   useEffect(() => {
     const query= '*[_type== "works"]';
     client.fetch(query)
       .then ((data) => {
         setWorks(data);
-        setfilterWork(data);
-      })
+        setFilterWork(data);
+      });
   }, [])
   
 
@@ -29,7 +29,7 @@ const Work = () => {
   return (
     <>
           <h2 className="head-text">My <span>projects </span></h2>
-      <div className="app__work-filter">
+      <div className="app__works-filter">
         {['UI/UX','Web App','Mobile App', 'React JS','All'].map((item, index) => (
         <div 
           key={index}
@@ -49,13 +49,13 @@ const Work = () => {
         {filterWork.map((work,index) =>(
             <div className="app__work-item app__flex" key={index}>
               <div className="app__work-img app__flex">
-                <img src={urlFor(work.imgUrl)} alt={work.name}/>
+               <img src={urlFor(work.imgUrl)} alt={work.name}/> 
                 <motion.div 
                 whileHover={{opacity: [0,1]}}
                 transition={{duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5}}
                 className="app__work-hover app__flex"
                 >
-                  <a href={work.projectLink} target="_blank" rel="norefer">
+                  <a href={work.projectLink} target="_blank" rel="noreferrer">
                     <motion.div
                     whileInView={{scale: [0,1]}}
                     whileHover={{scale: [1,0.9]}}
@@ -90,7 +90,7 @@ const Work = () => {
       </motion.div>
 
     </> 
-  )
-}
+  );
+};
 
 export default AppWrap(Work,'work');
